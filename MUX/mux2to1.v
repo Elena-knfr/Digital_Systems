7ec5635a6caf33f840811dfdf3ee4c6e852fa87e
ile-rfnk <56362099+ile-rfnk@users.noreply.github.com>
2021-01-15 22:59:07 -0500

    Update mux2to1.v

--- MUX/mux2to1.v
+++ MUX/mux2to1.v
@@ -1,3 +1,14 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+module top(SW, LEDR);
+    input [9:0]SW;
+    output [9:0]LEDR;
+    
+    mux2to1 u0(SW[0], SW[1], SW[9], LEDR[0]);
+
+endmodule
+
 module mux2to1(x, y, s, m);
     input x; //select 0
     input y; //select 1
