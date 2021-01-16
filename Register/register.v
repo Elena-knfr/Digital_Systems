8fdc9defdd33f2e420408a825fd764bbacff1412
ile-rfnk <56362099+ile-rfnk@users.noreply.github.com>
2021-01-15 23:09:46 -0500

    Update register.v

--- Register/register.v
+++ Register/register.v
@@ -1,3 +1,14 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+module top(SW, LEDR);
+	input [9:0]SW;
+	output [9:0]LEDR;
+
+	register u0(SW[7:0], SW[9], SW[8], LEDR[7:0]);
+
+endmodule
+
 module register(in, clock, resetn, out);
 	input [7:0]in;
 	input clock, resetn;
