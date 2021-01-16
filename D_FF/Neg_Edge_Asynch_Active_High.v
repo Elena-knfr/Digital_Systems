5ab7244647396f4f1a913c697d16e97b3073443d
ile-rfnk <56362099+ile-rfnk@users.noreply.github.com>
2021-01-15 22:41:08 -0500

    Update Neg_Edge_Asynch_Active_High.v

--- D_FF/Neg_Edge_Asynch_Active_High.v
+++ D_FF/Neg_Edge_Asynch_Active_High.v
@@ -1,3 +1,14 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+module top(SW, LEDR); 
+	input[9:0]SW;
+	output[9:0]LEDR;
+
+	NegEdge_AsyncActiveHigh u0(SW[0], SW[9], SW[1], LEDR[0]);
+
+endmodule
+
 
 module NegEdge_AsyncActiveHigh(d, clock, resetp, q);
     input d, clock, resetp;
