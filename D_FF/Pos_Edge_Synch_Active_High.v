87929baa98ac9d6e82dd90b3234aa81d73c19a84
ile-rfnk <56362099+ile-rfnk@users.noreply.github.com>
2021-01-15 22:45:31 -0500

    Update Pos_Edge_Synch_Active_High.v

--- D_FF/Pos_Edge_Synch_Active_High.v
+++ D_FF/Pos_Edge_Synch_Active_High.v
@@ -1,3 +1,13 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+module top(SW, LEDR); 
+	input[9:0]SW;
+	output[9:0]LEDR;
+
+	PosEdge_SyncActiveHigh u0(SW[0], SW[9], SW[1], LEDR[0]);
+
+endmodule
 
 module PosEdge_SyncActiveHigh(d, clock, resetp, q);
     input d, clock, resetp;
