b838dbca7cedb7eb9962c08fcdeef954f89aa70b
ile-rfnk <56362099+ile-rfnk@users.noreply.github.com>
2021-01-15 23:00:04 -0500

    Update mux2to1_2bit.v

--- MUX/mux2to1_2bit.v
+++ MUX/mux2to1_2bit.v
@@ -1,3 +1,14 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+module top(SW, LEDR);
+    input [9:0]SW;
+    output [9:0]LEDR;
+    
+	mux2to1_2bit u0(SW[1:0], SW[3:2], SW[9], LEDR[1:0]);
+
+endmodule
+
 module mux2to1_2bit(x, y, s, m);
 	input [1:0]x, y;
 	input s;
