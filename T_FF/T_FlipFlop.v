214d70c831b2fcf94e77d8c9eb0c44b6679259a7
ile-rfnk <56362099+ile-rfnk@users.noreply.github.com>
2021-01-18 13:39:39 -0500

    Update T_FlipFlop.v

--- T_FF/T_FlipFlop.v
+++ T_FF/T_FlipFlop.v
@@ -1,3 +1,13 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+module top(SW, LEDR); 
+	input[9:0]SW;
+	output[9:0]LEDR;
+
+	T_FF u0(SW[0], SW[9], SW[1], LEDR[0]);
+
+endmodule
 
 module T_FF(Clock, Clear_b, T, Q);
 	input Clock, Clear_b, T;
